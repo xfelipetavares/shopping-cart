c31b71f1c6ab9e6291667eb50c95adf538f80c19
Felipe Tavares <xfelipetavares@gmail.com>
2022-11-18 00:15:50 -0300

    exercise 4 done

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 
 const products = document.querySelector('.products');
 
+export const showLoadingScreen = (item) => {
+  const loadingScreen = document.createElement('p');
+  loadingScreen.classList.add('loading');
+  loadingScreen.innerText = 'carregando...';
+  item.appendChild(loadingScreen);
+};
+export const removeLoadingScreen = (item) => {
+  item.innerHTML = '';
+};
+
 async function showProducts() {
+  showLoadingScreen(products);
   const product = await fetchProductsList('computador');
+  removeLoadingScreen(products);
   product.forEach((element) => {
     products.appendChild(createProductElement(element));
   });
